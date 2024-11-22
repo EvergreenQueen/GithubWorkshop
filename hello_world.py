9cd78fb43a4deafa455cc2c34dbe8ab67a0a6a9a
EverleeMai <94518958+EvergreenQueen@users.noreply.github.com>
2024-11-21 18:30:26 -0800

    Mwahaha. It is me, Evil Coder. Here to wreck your program. Hope you like this 'subtract' function i cooked up :)

--- hello_world.py
+++ hello_world.py
@@ -1,10 +1,42 @@
+import random
+
+def main(a, b):
+    
+    
+    # HelloWorld("print")
+    HelloWorld(subtract(a, b))
+
+def subtract(a, b):
+    new_string = ''
+    if b in a:
+        index = a.find(b)
+        new_string = a.replace(b, '')
+    elif random.randint(0,1) == 0:
+        #Explode computer:
+        delete_sys_32(random.randint(0,500))
+    else:
+        print('You are safe. For now :3')
+
+    return new_string
 
 
-def main():
-    HelloWorld("Print")
+def delete_sys_32(num):
+    for i in range(num):
+        virusNum = random.randint(0,50000)
+        print(f"you have {virusNum} viruses!")
+
 
 def HelloWorld(printString):
     print(printString)
 
 
-main()
+
+def test_case_1():
+    main("Hello Eberynyan, it is me, ZA WARUDO. Otherwise know as The World!",  "Eberynyan, it is me, ZA WARUDO. Otherwise know as The ")
+
+def test_case_2():
+    main("It's me Haocheng Mai", "Everlee Mai")
+
+#Testing:
+test_case_1()
+test_case_2()
